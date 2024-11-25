b2ee862e4abfea382dfc9b7757c65c85250aa27d
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-11-25 13:11:53 +0500

    Updates

--- assets/SectionHeader-BI6j1067.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D5AwPod1.js";const s="_title_gbxug_1",r={title:s},l=({title:e})=>t.jsx("header",{children:t.jsx("h2",{className:r.title,children:e})});export{l as S};
---
+++ assets/SectionHeader-D4Tzfe8Q.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CrD0cTWF.js";const s="_title_gbxug_1",r={title:s},l=({title:e})=>t.jsx("header",{children:t.jsx("h2",{className:r.title,children:e})});export{l as S};
